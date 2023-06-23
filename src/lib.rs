2401f10817fe2774f57ef08e0f0b147edcb2aa18
Dogue <dogue@oddhelix.com>
2023-06-22 20:17:11 -0400

    doc: add doc comments, bump 0.2.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,76 @@
+#![crate_name = "ncspot_types"]
+
+//! This crate provides a set of types that derive `serde::Deserialize` that match the JSON
+//! output from [ncspot](https://github.com/hrkfdn/ncspot).
+//!
+//! Interior types are matched as closely as possible to the way the data is modelled inside
+//! of ncspot, with one exception: `duration` is deserialized as a `std::time::Duration` whereas
+//! internally, ncspot represents this as a `u32`.
+//!
+//! I'm not entirely convinced this is necessary yet, but it may be convenient when using the library.
+
 use chrono::offset::Utc;
 use chrono::DateTime;
 use serde::Deserialize;
 use std::time::Duration;
 
-#[derive(Debug, Deserialize)]
-pub struct Status {
+/// A single event from ncspot
+#[derive(Debug, Deserialize, Clone)]
+pub struct Event {
     pub mode: Mode,
     pub playable: Playable,
 }
 
-#[derive(Debug, Deserialize)]
+/// Type of event, [Track] or [Episode]
+#[derive(Debug, Deserialize, Clone)]
 #[serde(untagged)]
 pub enum Playable {
     Track(Track),
     Episode(Episode),
 }
 
-#[derive(Debug, Deserialize)]
+/// Current playback mode
+#[derive(Debug, Deserialize, Clone, Copy)]
 pub enum Mode {
+    /// A [Track] or [Episode] is currently playing
     Playing {
         secs_since_epoch: usize,
         nanos_since_epoch: usize,
     },
 
-    Paused {
-        secs: usize,
-        nanos: usize,
-    },
+    /// A [Track] or [Episode] is currently paused
+    Paused { secs: usize, nanos: usize },
 
+    /// Emitted briefly when playing a chosen song
+    /// before then emitting a `Playing` event
     Stopped,
 }
 
+/// A music track
 #[serde_with::serde_as]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Track {
-    pub id: String,
+    pub id: Option<String>,
     pub uri: String,
     pub title: String,
-    pub track_number: usize,
-    pub disc_number: usize,
+    pub track_number: u32,
+    pub disc_number: i32,
     #[serde_as(as = "serde_with::DurationMilliSeconds<u64>")]
     pub duration: Duration,
     pub artists: Vec<String>,
     pub artist_ids: Vec<String>,
-    pub album: String,
-    pub album_id: String,
+    pub album: Option<String>,
+    pub album_id: Option<String>,
     pub album_artists: Vec<String>,
-    pub cover_url: String,
+    pub cover_url: Option<String>,
     pub url: String,
     pub added_at: Option<DateTime<Utc>>,
     pub list_index: usize,
 }
 
+/// A podcast episode
 #[serde_with::serde_as]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Episode {
     pub id: String,
     pub uri: String,
